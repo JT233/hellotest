0a049cb0edd61b0f598f634b4b8e0d28f8ffeb39
JT233 <2323784792@qq.com>
2019-12-24 15:41:29 +0800

    第二次提交

---
+++ src/com/zking/text/Test2.java
@@ -0,0 +1,12 @@
+package com.zking.text;/*
+ *@author J.T
+ *@company xxxxx
+ *@site www.xxxx.com
+ *@create 2019-12-24-{TIME}
+ */
+
+public class Test2 {
+    public static void main(String[] args){
+        System.out.println("test 2");
+    }
+}
